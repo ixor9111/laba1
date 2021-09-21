8279bf929a4c110566eadab2ace61df4eac099d4
Максим Микитюк <maks.mykytyuk@gmail.com>
2021-09-21 17:51:03 +0300

    підключив до бд azure

--- Models/laba1Context.cs
+++ Models/laba1Context.cs
@@ -8,14 +8,26 @@ namespace laba1.Models
 {
     public class laba1Context : DbContext
     {
+        public laba1Context()
+        {
+        }
+
+        public laba1Context(DbContextOptions<laba1Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:servercyb.database.windows.net,1433;Initial Catalog=laba1;Persist Security Info=False;User ID=someks;Password=17aB21135kSssV1980;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
         public virtual DbSet<Employee> Employee { get; set; }
         public virtual DbSet<File> File { get; set; }
         public virtual DbSet<Department> Department { get; set; }
         public virtual DbSet<Work> Work { get; set; }
-
-        public laba1Context(DbContextOptions<laba1Context> options) : base(options)
-        {
-            Database.EnsureCreated();
-        }
     }
 }
