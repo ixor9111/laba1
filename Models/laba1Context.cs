bbfaabb8bd17c3016b0bc2d2c2ed3ea4f9ec5066
Максим Микитюк <maks.mykytyuk@gmail.com>
2021-09-21 18:15:57 +0300

    прибрав зайве

--- Models/laba1Context.cs
+++ Models/laba1Context.cs
@@ -17,14 +17,6 @@ namespace laba1.Models
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Server=tcp:servercyb.database.windows.net,1433;Initial Catalog=laba1;Persist Security Info=False;User ID=someks;Password=17aB21135kSssV1980;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         public virtual DbSet<Employee> Employee { get; set; }
         public virtual DbSet<File> File { get; set; }
         public virtual DbSet<Department> Department { get; set; }
