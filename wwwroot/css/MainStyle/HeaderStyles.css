b01aafd5cc2e8abaa28041e533c23cb8b0af8c25
igorbutenko9111 <igorbutenko9111@gmail.com>
2021-10-27 10:55:26 +0300

    Изменил header

---
+++ wwwroot/css/MainStyle/HeaderStyles.css
@@ -0,0 +1,23 @@
+﻿.prevImg {
+	height: 50px;
+	width: 50px;
+}
+.nav-item, .nav-item-right {
+	color: #4a4a4a;
+	font-weight: 600 !important;
+	text-decoration: none;
+	line-height: 1.5;
+	font-size: 1.2rem;
+}
+.nav-item-right {
+	margin-left: auto;
+}
+.nav-item:hover {
+	color: #00C0FF;
+}
+.nav-space{
+	width: 10%;
+}
+body {
+	color: #4a4a4a;
+}
