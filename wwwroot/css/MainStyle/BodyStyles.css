c3baa1ccec36a582421ea4faee8a916ab740db04
igorbutenko9111 <igorbutenko9111@gmail.com>
2021-10-27 17:32:08 +0300

    реализовал details index

---
+++ wwwroot/css/MainStyle/BodyStyles.css
@@ -0,0 +1,58 @@
+﻿a{
+	color: none;
+}
+.createButton {
+	border-radius: 5px;
+	font-size: 1.05em;
+	color: #4a4a4a;
+	padding: 8px 17px;
+	font-family: sans-serif;
+	border: 0px;
+	margin-top: 10px;
+}
+	.createButton:hover {
+		transition-duration: 0.1s;
+		background-color: rgb(224 224 224);
+	}
+.employeeImg {
+	height: 200px;
+	width: 200px;
+	border-radius: 20px;
+}
+.employeeElem {
+	float: left;
+	padding: 5px;
+	border-radius: 10px;
+	border: aliceblue;
+	border-style: double;
+}
+	.employeeElem:hover {
+		background-color: rgb(250 250 250);
+		transition-duration: 0.05s;
+	}
+
+.employeeElemTop {
+	width: 200px;
+	height: 200px;
+}
+.employeeElemBot {
+	width: 200px;
+	height: 100px;
+	text-align: center;
+	font-size: 1.4em;
+}
+.detailsLeft {
+	float: left;
+	margin: 0 20px 30px 0;
+	height: 300px;
+	width: 300px;
+}
+.detailsPhoto {
+	height: 300px;
+	width: 300px;
+}
+.detailsRight {
+	height: 330px;
+	float: left;
+	width: auto;
+}
